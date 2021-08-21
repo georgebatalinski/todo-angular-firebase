29a2b021f659022d2c6d23a83a8ff854110ea1ce
George Batalinski <george.batalinski@gmail.com>
2021-08-21 07:37:26 -0400

    --migrate-only --from=1.2.0

--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,14 @@ module.exports = function (config) {
   config.set({
     basePath: '',
 
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
 
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
 
     client:{
@@ -17,13 +17,11 @@ module.exports = function (config) {
     },
 
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
 
-    angularCli: {
-      environment: 'dev'
-    },
+    
 
     reporters: ['progress', 'kjhtml'],
     port: 9876,
